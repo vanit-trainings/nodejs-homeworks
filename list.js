093903ba24013d2a4736960fabc9cd131ccd8620
Narek <narekayd091@gmail.com>
2019-04-23 17:30:31 +0400

    homework

---
+++ list.js
@@ -0,0 +1,81 @@
+var node = function(val){
+	this.next = null;
+	this.prev = null;
+	this.value = val;
+}
+
+var list = function(){
+	this.length = 0;
+	this.first = null;
+	this.last = null;
+}
+
+list.prototype.add = function(data){
+	var n = new node(data);
+	if(this.first == null && this.last === null){
+		this.last = n;
+		this.first = n;
+	}
+	else{
+		this.last.next = n;
+		n.prev = this.last;
+		this.last = n;
+	}
+	this.length++;
+}
+
+list.prototype.remove = function(ind){
+    if(!isNaN(Number(ind)) && ind >= 0 && ind < this.length){
+        let n = this.first;
+        if(ind === 0){
+            n.next.prev = null;
+            this.first = n.next;
+            n.prev = null;
+            n.next = null;
+            
+        }
+        else if(ind === (this.length - 1)){
+            n = this.last;
+            n.prev.next = null;
+            this.last = n.prev;
+            n.prev = null;
+        }
+        else{
+            for(let i = 0; i < ind; i++){
+                n = n.next;
+            }
+                n.prev.next = n.next;
+                n.next.prev = n.prev;
+                n.prev = null;
+                n.next = null;        
+        }
+        this.length--;
+    }
+}
+
+list.prototype.isEmpty = function(){
+	if(this.length === 0){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+
+list.prototype.print = function(){
+	let n = this.first;
+	while(n !== null){
+        console.log(n.value);
+		n = n.next;
+    }
+    console.log("");
+}
+
+var d = new list();
+d.add(1);
+d.add(2);
+d.add(3);
+d.add(4);
+d.print();
+d.remove(2);
+d.print();

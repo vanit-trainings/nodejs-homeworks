18010bb973fb02b1fd3ae346882af3e974896bb5
Karen <manukyankaren00@gmail.com>
2019-04-24 20:29:34 +0400

    no token

--- Karen/regLog/routes/users.js
+++ Karen/regLog/routes/users.js
@@ -6,15 +6,18 @@ var uniqid = require('uniqid');
 router.post('/register', function(req, res) {
 	fs.readFile("./data/data.json", "utf8", function(err, data){
 		if(err) {return res.status(500).send("Internal Server Error")};
-		if(!req.body) {return res.status(404).send("Record Not Found")};
 		const users = JSON.parse(data);
 		const newUserId = {};
-		const newId = uniqid.process(req.body.password);
+		const newId = uniqid.process();
 		const newUser = {
 			login: req.body.login,
 			password: req.body.password
 		};
 		if(!req.body.login || !req.body.password) {return res.status(400).send("mutq login")};
+		const x = /^\w+@\w+(\.\w{2,3})/;
+		const y = /^\w{8,16}/;
+		if(!x.test(req.body.login)) {return res.status(400).send("no corect email")};
+		if(!y.test(req.body.password)) {return res.status(400).send("no corect password")};
 		for(key in users){
 			if(users[key].login == req.body.login){
 				return res.status(400).send("This login already exists.");
@@ -24,9 +27,26 @@ router.post('/register', function(req, res) {
 		const user = Object.assign(users, newUserId);
 		fs.writeFile("./data/data.json", JSON.stringify(users), function(err) {
     		if(err) {return res.status(500).send("Internal Server Error")};
-    		res.send(newUserId);
+    		res.send("Registration Completed Successfully");
     	}); 
 
+	})
+});
+
+router.post('/login', function(req, res){
+	fs.readFile("./data/data.json", "utf8", function(err, data){
+		if(err) {return res.status(500).send("Internal Server Error")};
+		const users = JSON.parse(data);
+		const x = /^\w+@\w+(\.\w{2,3})/;
+		const y = /^\w{8,16}/;
+		if(!x.test(req.body.login)) {return res.status(400).send("no corect email")};
+		if(!y.test(req.body.password)) {return res.status(400).send("no corect password")};
+		for(key in users){
+			if((users[key].login === req.body.login) && (users[key].password === req.body.password)) {
+				return res.send('mutq');
+			}
+		}
+		res.status(400).send('no corect')
 	})
 });
 

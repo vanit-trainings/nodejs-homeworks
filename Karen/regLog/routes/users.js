b16e5df205aa0069cd5d8b43ce3ff7779278f621
Karen <manukyankaren00@gmail.com>
2019-04-24 15:48:43 +0400

    add

--- Karen/regLog/routes/users.js
+++ Karen/regLog/routes/users.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var uniqid = require('uniqid');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/register', function(req, res) {
+	fs.readFile("./data/data.json", "utf8", function(err, data){
+		if(err) {return res.status(500).send("Internal Server Error")};
+		if(!req.body) {return res.status(404).send("Record Not Found")};
+		const users = JSON.parse(data);
+		const newUserId = {};
+		const newId = uniqid.process(req.body.password);
+		const newUser = {
+			login: req.body.login,
+			password: req.body.password
+		};
+		if(!req.body.login || !req.body.password) {return res.status(400).send("mutq login")};
+		for(key in users){
+			if(users[key].login == req.body.login){
+				return res.status(400).send("This login already exists.");
+			}
+		};
+		newUserId[newId] = newUser;
+		const user = Object.assign(users, newUserId);
+		fs.writeFile("./data/data.json", JSON.stringify(users), function(err) {
+    		if(err) {return res.status(500).send("Internal Server Error")};
+    		res.send(newUserId);
+    	}); 
+
+	})
 });
 
 module.exports = router;

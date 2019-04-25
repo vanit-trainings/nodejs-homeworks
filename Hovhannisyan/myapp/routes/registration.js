97079df80b47a1f1ad8ecb8cb1c6e19a0940052d
narek1998 <narekna1998@gmail.com>
2019-04-25 18:30:11 +0400

    Homework Login and Registration

--- Hovhannisyan/myapp/routes/registration.js
+++ Hovhannisyan/myapp/routes/registration.js
@@ -4,40 +4,44 @@ const jsonfile = require('jsonfile');
 const filepath = "./data/users.json";
 function validateUsername(username) {
     var usernameRegex = /^[a-zA-Z\-]+$/;
-    return usernameRegex.test(username);
+    return usernameRegex.test(username)
 
 }
 function validatePass(password){
-    var passw=  /^[A-Za-z]\w{7,15}$/;
-    return passw.test(password);
+    var passw = /^[A-Za-z]\w{7,15}$/;
+    return passw.test(password)
 }
+function validateEmail(email){
+    var mail = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return mail.test(email)
+}   
 
 router.post('/', function (req, res) {
     jsonfile.readFile(filepath, function (err, obj) {
         if (err) {
             return res.status(500).send("Server error");
         }
-        if (!req.body.email) {
+        if (!validateUsername(req.body.username) || !validatePass(req.body.password) ||  !validateEmail(req.body.email)) {
             return res.status(400).send("bad request");
         }
-        if (!validateUsername(req.body.username) && !validatePass(req.body.password)) {
+        if(Object.keys(req.body).length !==3){
             return res.status(400).send("bad request");
         }
-              
         if (obj[req.body.username]) {
-            return res.status(400).send("Username is already used");
+            return res.status(400).send("Username is already existed");
         }
         
         obj[req.body.username] = {
+            username : req.body.username,
             password: Buffer.from(req.body.password).toString('base64'),//kodavorel
             email: req.body.email
+
         };
-        const porc = Buffer.from(obj[req.body.username].password,'base64').toString('ascii');
-        jsonfile.writeFile(filepath, obj, function (err) {
+        jsonfile.writeFile(filepath, obj, { spaces: 4, EOL: '\r\n' }, function (err) {
             if (err) {
                 return res.status(500).send("Server error");
             }
-            return res.status(200).send(porc);
+            return res.status(200).send("ok");
 
         });
     })

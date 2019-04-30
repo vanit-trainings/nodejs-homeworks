fde5444ac91c6b6ed20b907282c32879a3aab5d9
narek1998 <narekna1998@gmail.com>
2019-04-30 21:15:30 +0400

    are used by eslint

--- Hovhannisyan/routes/login.js
+++ Hovhannisyan/routes/login.js
@@ -1,97 +1,105 @@
 const express = require('express');
+
 const router = express.Router();
 const jsonfile = require('jsonfile');
-const filepath = "./data/users.json";
-const loginedUsers = "./data/loginedUsers.json";
+
+const filepath = './data/users.json';
+const loginedUsers = './data/loginedUsers.json';
 const crypto = require('crypto');
+
 const hash = crypto.createHash('sha512');
 
-function toCode(a){
-    data = hash.update(a, 'utf-8');
-    gen_hash= data.digest('hex');
-    return "hash : " + gen_hash;
-}
+const tokenSize = 32;
 
-function tokenGenerate() {
-    var string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789.@-_';
-    var token = '';
-    for (let i = 0; i < 32; i++) {
-        token += string[Math.floor(Math.random() * string.length)];
+const toCode = (a) => {
+    const data = hash.update(a, 'utf-8');
+    const genHash = data.digest('hex');
+
+    return `hash : ${genHash}`;
+};
+
+const tokenGenerate = () => {
+    const string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789.@-_';
+
+    let token = '';
+
+    for (let i = 0; i < tokenSize; i++) {
+        token += string[ Math.floor(Math.random() * string.length) ];
     }
     return token;
-}
+};
 
 
-router.post('/', function (req, res) {
-
-    jsonfile.readFile(filepath, function (err, obj) {
-        if (err) {
-            return res.status(500).send("Server error!");
+router.post('/', (req, res) => {
+    jsonfile.readFile(filepath, (err1, obj) => {
+        if (err1) {
+            return res.status(500).send('Server error!');
         }
         if (Object.keys(req.body).length !== 2 || !req.body.username || !req.body.password) {
-            return res.status(400).send("Bad request");
+            return res.status(400).send('Bad request');
         }
-        if(!(toCode(req.body.password) === obj[req.body.username].password)){
-            return res.status(400).send("Bad request")
-        }        
-        /*if (!(Buffer.from(obj[req.body.username].password, 'base64').toString('ascii') === req.body.password)) {
+        if (!(toCode(req.body.password) === obj[ req.body.username ].password)) {
+            return res.status(400).send('Bad request');
+        }
+        /* if (!(Buffer.from(obj[req.body.username].password, 'base64').toString('ascii') === req.body.password)) {
             return res.status(400).send("Bad request");
         }*/
-        if (!obj[req.body.username]) {
-            return res.status(400).send("Bad request");
+        if (!obj[ req.body.username ]) {
+            return res.status(400).send('Bad request');
         }
 
-        jsonfile.readFile(loginedUsers, function (err, data) {
-            if (err) {
-                return res.status(500).send("Server error!");
+        jsonfile.readFile(loginedUsers, (err2, data) => {
+            if (err2) {
+                return res.status(500).send('Server error!');
             }
-            if (data[req.headers.token]) {
-                return res.status(200).send("Has already logined")
+            if (data[ req.headers.token ]) {
+                return res.status(200).send('Has already logined');
             }
-            let token = tokenGenerate();
+            const token = tokenGenerate();
             
-            let later = (new Date()).getTime() + (2*1000*60*60);
+            const later = (new Date()).getTime() + (2 * 1000 * 60 * 60);
             
-            data[token] = {
-                token: token,
+            data[ token ] = {
+                token,
                 username: req.body.username,
                 date: later
-            }
-            //if(req.headers.token.date === later.setHours(now.getHours() + 6)){
+            };
+            // if(req.headers.token.date === later.setHours(now.getHours() + 6)){
             //  token = tokenGenerate();
-            //}
-            jsonfile.writeFile(loginedUsers, data, { spaces: 4, EOL: '\r\n' }, (err) => {
-                if (err) {
-                    return res.status(500).send("Server error!");
+            // }
+            jsonfile.writeFile(loginedUsers, data, { spaces: 4, EOL: '\r\n' }, (err3) => {
+                if (err3) {
+                    return res.status(500).send('Server error!');
                 }
-                return res.status(200).send("You are logined");
-            })
-        })
-    })
-})
-router.get('/authorized',function(req,res){
-    if(!req.headers.token){
-        return res.status(401).send("User is'nt unautherized!");
+                return res.status(200).send('You are logined');
+            });
+        });
+    });
+});
+router.get('/authorized', (req, res) => {
+    if (!req.headers.token) {
+        return res.status(401).send('User is\'nt unautherized!');
     }
-    jsonfile.readFile(loginedUsers,function(err,data){
-        if(err){
-            return res.status(500).send("Server error!");
+    jsonfile.readFile(loginedUsers, (err, data) => {
+        if (err) {
+            return res.status(500).send('Server error!');
         }
-        if(data[req.headers.token] === undefined){
-            return res.status(401).send("User is'nt unauthorized!");
+        if (data[ req.headers.token ] === undefined) {
+            return res.status(401).send('User is\'nt unauthorized!');
         }
-        if(data[req.headers.token].date < (new Date()).getTime() ){
-            return res.status(401).send("User isn't unauthorized")
+        if (data[ req.headers.token ].date < (new Date()).getTime()) {
+            return res.status(401).send('User isn\'t unauthorized');
         }
-        jsonfile.readFile(filepath,function(err,obj){
-            if(err){
-                return res.status(500).send("Server error!");
+        jsonfile.readFile(filepath, (err1, obj) => {
+            if (err1) {
+                return res.status(500).send('Server error!');
             }
-            let user = obj[data[req.headers.token].username];
+            const user = obj[ data[ req.headers.token ].username ];
+
             delete user.password;
             return res.status(200).send(user);
-        })
-    })
-})
+        });
+    });
+});
 
 module.exports = router;
--- Hovhannisyan/routes/registration.js
+++ Hovhannisyan/routes/registration.js
@@ -1,64 +1,64 @@
 
 const express = require('express');
+
 const router = express.Router();
 const jsonfile = require('jsonfile');
-const filepath = "./data/users.json";
-const crypto = require('crypto');
-const hash = crypto.createHash('sha512');
-function validateUsername(username) {
-    var usernameRegex = /^[a-zA-Z\-]+$/;
-    return usernameRegex.test(username)
 
+const filepath = './data/users.json';
+const crypto = require('crypto');
+
+const hash = crypto.createHash('sha512');
+
+function validateUsername(username) {
+    const usernameRegex = /^[a-zA-Z\-]+$/;
+
+    return usernameRegex.test(username);
 }
-function validatePass(password){
-    var passw = /^[A-Za-z]\w{7,15}$/;
-    return passw.test(password)
+function validatePass(password) {
+    const passw = /^[A-Za-z]\w{7,15}$/;
+
+    return passw.test(password);
 }
-function validateEmail(email){
-    var mail = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return mail.test(email)
-}   
-function toCode(a){
+function validateEmail(email) {
+    const mail = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    return mail.test(email);
+}
+function toCode(a) {
     data = hash.update(a, 'utf-8');
-    gen_hash= data.digest('hex');
-    return "hash : " + gen_hash;
+    gen_hash = data.digest('hex');
+    return `hash : ${gen_hash}`;
 }
-router.post('/', function (req, res) {
-    jsonfile.readFile(filepath, function (err, obj) {
+router.post('/', (req, res) => {
+    jsonfile.readFile(filepath, (err, obj) => {
         if (err) {
-            return res.status(500).send("Server error");
+            return res.status(500).send('Server error');
         }
-        if (!validateUsername(req.body.username) || !validatePass(req.body.password) ||  !validateEmail(req.body.email)) {
-            return res.status(400).send("bad request");
+        if (!validateUsername(req.body.username) || !validatePass(req.body.password) || !validateEmail(req.body.email)) {
+            return res.status(400).send('bad request');
         }
-        if(Object.keys(req.body).length !==3){
-            return res.status(400).send("bad request");
+        if (Object.keys(req.body).length !== 3) {
+            return res.status(400).send('bad request');
         }
-        if (obj[req.body.username]) {
-            return res.status(400).send("Username is already existed");
+        if (obj[ req.body.username ]) {
+            return res.status(400).send('Username is already existed');
         }
         
-        obj[req.body.username] = {
-            username : req.body.username,
-            password : toCode(req.body.password),
-            //password: Buffer.from(req.body.password).toString('base64'),//kodavorel
+        obj[ req.body.username ] = {
+            username: req.body.username,
+            password: toCode(req.body.password),
+            // password: Buffer.from(req.body.password).toString('base64'),//kodavorel
             email: req.body.email
 
         };
-        jsonfile.writeFile(filepath, obj, { spaces: 4, EOL: '\r\n' }, function (err) {
+        jsonfile.writeFile(filepath, obj, { spaces: 4, EOL: '\r\n' }, (err) => {
             if (err) {
-                return res.status(500).send("Server error");
+                return res.status(500).send('Server error');
             }
-            return res.status(200).send("ok");
-
+            return res.status(200).send('ok');
         });
-    })
-
+    });
 });
-
-
-
-
 
 
 module.exports = router;

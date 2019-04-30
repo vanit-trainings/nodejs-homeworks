8dae302935f688a1bc64ab2f1b53040d47977c11
astgh13 <astgharakelyan97@gmail.com>
2019-04-30 18:42:05 +0400

    + getUserInfo

--- Astgh/routes/account.js
+++ Astgh/routes/account.js
@@ -56,6 +56,25 @@ function validateName(name) {
     return false;
 }
 
+function validations(req) {
+    if (Object.keys(req.body).length === 0) {
+		return 'Bad request: Body is empty';
+	}
+	if (!validateName(req.body.firstName) || !validateName(req.body.lastName)) {
+		return 'Bad request: Invalid firstName or lastName';
+	}
+	if (!validateEmail(req.body.email)) {
+		return 'Bad request: Enter valid email';
+	}
+	if (!validateLogin(req.body.login) && !validateEmail(req.body.login)) {
+		return 'Bad request: Enter valid login';
+	}
+	if (!validatePassword(req.body.password)) {
+		return 'Bad request: Enter valid password';
+    }
+    return 'OK';                                             
+}
+
 function authorization(req) {
     const token = req.headers['token'];
     let id;
@@ -76,39 +95,27 @@ function authorization(req) {
             return 'Server error';
         }
         if (obj3[token] !== id){
-            return "Token Id pair don't match";
+            return 'Token-Id pair not found';
         }
         return 'OK';
     });
 }
 
 router.post('/registr', function(req, res) {
-	if (Object.keys(req.body).length === 0) {
-		return res.status(400).send('Bad request: Body is empty');
-	}
-	if (!validateName(req.body.firstName) || !validateName(req.body.lastName)) {
-		return res.status(400).send('Bad request: Invalid firstName or lastName');
-	}
-	if (!validateEmail(req.body.email)) {
-		return res.status(400).send('Bad request: Enter valid email');
-	}
-	if (!validateLogin(req.body.login) && !validateEmail(req.body.login)) {
-		return res.status(401).send('Bad request: Enter valid login');
-	}
-	if (!validatePassword(req.body.password)) {
-		return res.status(400).send('Bad request: Enter valid password');
-	} 
-	jsonfile.readFile(filePath, function(err, obj) {
+    if (validations(req) ===  'Bad request: Body is empty') {
+        return res.status(Status.statusCode).send(Status.statusMessage);
+    }
+        jsonfile.readFile(filePath, function(err, obj) {
 		if (err) {
 			return res.status(500).send('Server error');
 		}
 		if (!existingEmail(obj, req.body.email)) {
 			return res.status(409).send('Bad request: Email already busy'); 
-		}
+        }
 		jsonfile.readFile(logPassPath, function(err, obj1) {
 			if (err) {
 				return res.status(500).send('Server error');
-			}
+            }
 			if (!existingLogin(req.body.login, obj1)) {
 				return res.status(400).send('Bad request: Login already busy'); 
 			}
@@ -143,7 +150,7 @@ router.post('/registr', function(req, res) {
 				}
 			});
 		});
-	});
+    })
 });
 
 router.post('/login', function(req, res) {
@@ -170,44 +177,25 @@ router.post('/login', function(req, res) {
                 if (err) {
                     return res.status(500).send('Server error'); 
                 }
-                
                 obj5[token] = id;
                 jsonfile.writeFile(tokenIdPath, obj5, {spaces: 2, EOL: "\r\n"}, function(err) {
                     if (err) {
                         return res.status(500).send('Server error');
                     }
-                    jsonfile.readFile(filePath, function(err, obj5) {
-                        if (err) {
-                            return res.status(500).send('Server error'); 
-                        }
-                        let userInfo = {};
-                        userInfo.id = obj5[id].userId;
-                        userInfo.firstName = obj5[id].firstName;
-                        userInfo.lastName = obj5[id].lastName;
-                        userInfo.birthDate = obj5[id].birthDate;
-                        userInfo.gender = obj5[id].gender;
-                        userInfo.email = obj5[id].email;
-                        jsonfile.writeFile(filePath, obj5,{spaces: 2, EOL: "\r\n"}, function(err) {
-                        if (err) {            
-                            return res.status(500).send('Server error');
-                        }
                         jsonfile.writeFile(logPassPath, obj4, {spaces: 2, EOL: "\r\n"}, function(err) {
                             if (err) {
                                 return res.status(500).send('Server error');
                             }
                             res.writeHead(200, {'token':token});
-                            let jsStr = JSON.stringify(userInfo);
-                            res.write(jsStr);
+                            res.write('OK');
                             res.end();
                             return res.send();
                             });
                         });    
                     });
-                });
+                }
             });
-        }
     });
-});
 
 router.get('/logOut', function(req, res) {
     if (authorization(req) === 'Server error') {
@@ -216,8 +204,8 @@ router.get('/logOut', function(req, res) {
     if (authorization(req) === 'Id is missing') {
         return res.status(400).send('Bad request: UserId is missing');
     }
-    if (authorization(req) === "Token Id pair don't match") {
-        return res.status(400).send("Bad request: Token Id pair don't match");
+    if (authorization(req) === 'Token-Id pair not found') {
+        return res.status(400).send('Bad request: Token-Id pair not found');
     }
     const token = req.headers['token'];
     jsonfile.readFile(tokenIdPath, function(err, obj6) {
@@ -230,6 +218,51 @@ router.get('/logOut', function(req, res) {
                 return res.status(500).send('Server error'); 
             }
             return res.status(200).send('OK');
+        });
+    });
+});
+
+router.get('/userInfo', function(req, res) {
+    const userId = req.query.userId;
+    let id = req.query.clientId;
+    const token = req.headers['token'];
+    if (!id) {
+        id = userId;
+    }
+    if (authorization(req) === 'Server error') {
+        return res.status(500).send('Server error');
+    }
+    if (authorization(req) === 'Id is missing') {
+        return res.status(400).send('Bad request: UserId is missing');
+    }
+    if (authorization(req) === 'Token-Id pair not found') {
+        return res.status(400).send('Bad request: Token-Id pair not found');
+    }
+
+    jsonfile.readFile(filePath, function(err, obj5) {
+        if (err) {
+            return res.status(500).send('Server error'); 
+        }
+        let userInfo = {};
+        userInfo.id = obj5[id].userId;
+        userInfo.firstName = obj5[id].firstName;
+        userInfo.lastName = obj5[id].lastName;
+        userInfo.birthDate = obj5[id].birthDate;
+        userInfo.gender = obj5[id].gender;
+        userInfo.email = obj5[id].email;
+        jsonfile.writeFile(filePath, obj5, {spaces: 2, EOL: "\r\n"}, function(err) {
+        if (err) {            
+            return res.status(500).send('Server error');
+        }
+        if (!userInfo) {
+            return res.status(400).send('User not found');            
+        }
+        console.log(token);
+        res.writeHead(200, {"token" : token});
+        let info = JSON.stringify(userInfo);
+        res.write(String(info));
+        res.end();
+        return res.send();
         });
     });
 });

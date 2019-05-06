9204df580cc020f88f5a04fe4cb0ed0df71ef702
narek1998 <narekna1998@gmail.com>
2019-05-06 16:46:01 +0400

    Update baseModules

--- Hovhannisyan/myapp/baseModules/baseMod.js
+++ Hovhannisyan/myapp/baseModules/baseMod.js
@@ -1,44 +1,43 @@
 const express = require('express');
-//const filepath = require('/');
-//const router = express.Router();
 const router = express.Router();
 const jsonfile = require('jsonfile');
 const filepath = '../data/users.json';
-//const serverError = res.status(500).send('Server error');
-//const OK = res.status(200).send('ok');
+
 addItem = function (filepath, key, value) {
     jsonfile.readFile(filepath)
-    .then(data => { data[key] = value 
-    jsonfile.writeFile(filepath, data, { spaces: 4, EOL: '\r\n' });
-})
-.catch(err =>{console.log(err)});
+        .then(data => {
+        data[key] = value
+            jsonfile.writeFile(filepath, data, { spaces: 4, EOL: '\r\n' });
+        })
+        .catch(err => { console.log(err) });
 }
 deleteItem = function (filepath, id) {
     jsonfile.readFile(filepath)
-    .then(data => { delete data[id];
-    jsonfile.writeFile (filepath, data, { spaces: 4, EOL: '\r\n'});
-})
-    .catch(err =>{console.log(err)});
+        .then(data => {
+            delete data[id];
+            jsonfile.writeFile(filepath, data, { spaces: 4, EOL: '\r\n' });
+        })
+        .catch(err => { console.log(err) });
 }
 
 readItem = function (filepath, id) {
     jsonfile.readFile(filepath)
-    .then(data => { console.log( data[id] )})
-    .catch(err =>{console.log(err)});
+        .then(data => { console.log(data[id]) })
+        .catch(err => { console.log(err) });
 }
-readAll = function (filepath){
+readAll = function (filepath) {
     jsonfile.readFile(filepath)
-    .then(data => { return data })
-    .catch( err => {console.log(err)});
+        .then(data => { return data })
+        .catch(err => { console.log(err) });
 }
 
-updateItem = function (filepath, key, value){
+updateItem = function (filepath, key, value) {
     jsonfile.readFile(filepath)
-    .then(data => { data[key] = value;
-    jsonfile.writeFile(filepath, data, { spaces: 4, EOL: '\r\n' });
-})
-    .catch('serverError')
+        .then(data => {
+        data[key] = value;
+            jsonfile.writeFile(filepath, data, { spaces: 4, EOL: '\r\n' });
+        })
+        .catch('serverError')
 }
-console.log(readItem(filepath,1));
 
 

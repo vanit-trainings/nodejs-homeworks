5019d958ebf2ff635cb74495bf20088ec3db1ae5
QochinyanTigran <093550242tigo@gmail.com>
2019-05-20 17:47:19 +0400

    add change film

--- Tigran/RottenTomatoes/routes/search.js
+++ Tigran/RottenTomatoes/routes/search.js
@@ -4,16 +4,6 @@ const jsonfile = require('jsonfile');
 
 const base_model = require('./../base_model/base_model.js');
 
-const BoxOficeMovieList = "./data/Movie_lists/Box_ofice_movie_lists.json";
-const OpeningThisWeek = "./data/Movie_lists/opening_this_week.json";
-const UpcomingMoviesList = "./data/Movie_lists/upcoming_movies_list.json";
-const CertifiedFreshMosies = "./data/Movie_lists/certifed_fresh_movies.json";
-
-const UpcomingDvdList = "./data/DVD_lists/upcoming_dvd_list.json";
-const NewReleases = "./data/DVD_lists/new_releases.json";
-const TopDvd = "./data/DVD_lists/top_dvd.json";
-const CertifiedFreshDvd = "./data/DVD_lists/certified_fresh_dvd.json";
-
 const AllFilms = "./data/Films_info/all_fims_info.json";
 
 
